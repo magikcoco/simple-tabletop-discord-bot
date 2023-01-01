9792c70c1537c9b9048072fd94eee7cd29d95a93
homepc <punishedtomato@gmail.com>
2023-01-01 13:15:52 -0500

    Added connection to a mongoDB server to store permanent data

--- src/main/java/com/magikcoco/manager/DataManager.java
+++ src/main/java/com/magikcoco/manager/DataManager.java
@@ -1,5 +1,10 @@
 package com.magikcoco.manager;
 
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,10 +12,14 @@ public class DataManager {
 
     private static final DataManager INSTANCE = new DataManager();
     private List<ThreadManager> activeThreadManagers;
+    private MongoClient mongoClient; //the connection to the mongo server
+    private MongoDatabase mongoDatabase; //the database for storing information
 
     private DataManager(){
         //default constructor
         activeThreadManagers = new ArrayList<>();
+        mongoClient = new MongoClient(new MongoClientURI("mongodb://localhost:27017")); //default config for mongoserver
+        mongoDatabase = mongoClient.getDatabase("simple-tabletop-bot-data");
     }
 
     public static DataManager getInstance(){
