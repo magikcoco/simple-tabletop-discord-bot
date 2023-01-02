7bdef89a9e4cb984aaeb60338bd18c11bf4784d7
homepc <punishedtomato@gmail.com>
2023-01-02 18:41:17 -0500

    Added functionality to add and remove documents as threads are added and deleted.

--- src/main/java/com/magikcoco/manager/DataManager.java
+++ src/main/java/com/magikcoco/manager/DataManager.java
@@ -4,6 +4,8 @@ import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import org.bson.Document;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,12 +16,14 @@ public class DataManager {
     private List<ThreadManager> activeThreadManagers;
     private MongoClient mongoClient; //the connection to the mongo server
     private MongoDatabase mongoDatabase; //the database for storing information
+    private MongoCollection<Document> managedThreads; //the collection for managed threads
 
     private DataManager(){
         //default constructor
         activeThreadManagers = new ArrayList<>();
         mongoClient = new MongoClient(new MongoClientURI("mongodb://localhost:27017")); //default config for mongoserver
         mongoDatabase = mongoClient.getDatabase("simple-tabletop-bot-data");
+        managedThreads = mongoDatabase.getCollection("managed-threads");
     }
 
     public static DataManager getInstance(){
@@ -29,10 +33,15 @@ public class DataManager {
 
     public void addActiveManager(ThreadManager manager){
         activeThreadManagers.add(manager);
+        Document managedThread = new Document()
+                .append("thread-id",manager.getThread().getId())
+                .append("manager-type",manager.getClass().getName());
+        managedThreads.insertOne(managedThread);
     }
 
     public void removeActiveManager(ThreadManager manager){
         activeThreadManagers.remove(manager);
+        managedThreads.deleteOne(Filters.eq("thread-id",manager.getThread().getId()));
     }
 
     public List<ThreadManager> getActiveThreadManagers(){
